dbc230509218de82bf8e540cbd33502cdc91488f
jorge97710 <jazmitia_10@hotmail.com>
2019-04-20 12:45:08 -0600

    pruebas

--- 8. RNA-1.R
+++ 8. RNA-1.R
@@ -9,12 +9,13 @@ library(neural)
 library(dummy)
 library(neuralnet)
 
-
+perros5 <- read.csv("train.csv")
 porcentaje<-0.7
-datos<-iris
+datos<-perros5
 set.seed(123)
-
-
+prueba <- perros5[,3:15]
+prueba[14] <- perros5[24]
+datos<- prueba
 corte <- sample(nrow(datos),nrow(datos)*porcentaje)
 train<-datos[corte,]
 test<-datos[-corte,]
@@ -23,13 +24,13 @@ test<-datos[-corte,]
 # Red Neuronal con nnet
 #-------------------------------------------------
 
-modelo.nn2 <- nnet(Species~.,data = datos,subset = corte, size=2, rang=0.1,
-                   decay=5e-4, maxit=200) 
-prediccion2 <- as.data.frame(predict(modelo.nn2, newdata = test[,1:4]))
+modelo.nn2 <- nnet(AdoptionSpeed~.,data = datos,subset = corte, size=2, rang=0.1,
+                   decay=5e-4, maxit=400) 
+prediccion2 <- as.data.frame(predict(modelo.nn2, newdata = test[,1:13]))
 columnaMasAlta<-apply(prediccion2, 1, function(x) colnames(prediccion2)[which.max(x)])
 test$prediccion2<-columnaMasAlta #Se le añade al grupo de prueba el valor de la predicción
 
-cfm<-confusionMatrix(as.factor(test$prediccion2),test$Species)
+cfm<-confusionMatrix(test$prediccion2,test$AdoptionSpeed)
 cfm
 
 #-------------------------------------------------
@@ -49,17 +50,17 @@ cfmWeka
 # Red Neuronal con caret
 #-------------------------------------------------
 
-modeloCaret <- train(Species~., data=train, method="nnet", trace=F)
-test$prediccionCaret<-predict(modeloCaret, newdata = test[,1:4])
-cfmCaret<-confusionMatrix(test$prediccionCaret,test$Species)
+modeloCaret <- train(AdoptionSpeed~., data=train, method="nnet", trace=F)
+test$prediccionCaret<-predict(modeloCaret, newdata = test[,1:13])
+cfmCaret<-confusionMatrix(as.factor(test$prediccionCaret),as.factor(test$AdoptionSpeed))
 cfmCaret
 
 
 #-------------------------------------------------
 # Red Neuronal con NeuralNet
 #-------------------------------------------------
-train$y<-as.numeric(train$Species)
-test$y<-as.numeric(test$Species)
+train$y<-as.numeric(train$AdoptionSpeed)
+test$y<-as.numeric(test$AdoptionSpeed)
 
 modelo.nn <- neuralnet(y~Sepal.Length+Petal.Length, train[,c(1,3,6)], hidden = 2, rep = 3)
 plot(modelo.nn, newdata=test) #Sale un gr?fico por cada repetici?n, en este caso saldr?n 3 gr?ficos
